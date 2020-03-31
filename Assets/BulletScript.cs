fe963ecb8cf7cc12ad3b422ad41a9c4563f8d5f2
Haris Salman <haris.solo68@gmail.com>
2020-03-31 19:52:56 +0700

    bullet shoot:

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        Invoke("DestroySelf", .6f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.velocity = (new Vector2(15, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("ground"))
+        {
+            DestroySelf();
+        }
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -4,77 +4,82 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
-    Animator animator;
-    Rigidbody2D rigidBody;
-    SpriteRenderer spriteRenderer;
 
-    [SerializeField]
-    private float runSpeed = 5f;
-    [SerializeField]
-    private float jumpSpeed = 10f;
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+    Object bulletRef;
 
     bool isGrounded;
-
     [SerializeField]
     Transform groundCheck;
     [SerializeField]
     Transform groundCheckL;
     [SerializeField]
     Transform groundCheckR;
+    [SerializeField]
+    private float runSpeed = 5f;
+    [SerializeField]
+    private float jumpSpeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        rigidBody = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        bulletRef = Resources.Load("Bullet");
+
     }
 
-    // Update is called once per frame
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
-        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"))||
+        string queuedAnim = "Player_idle";
+        isGrounded = false;
+
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||
             Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground")) ||
             Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")))
-
         {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-            animator.Play("Player_jump");
+            if (rb2d.velocity.y <= 1)
+            {
+                isGrounded = true;
+            }
         }
 
-            if (Input.GetKey("d") || Input.GetKey("right"))
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
-
-            if(isGrounded)
-                animator.Play("Player_run");
-
+            rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
+            queuedAnim = "Player_run";
             spriteRenderer.flipX = false;
         }
-        else if(Input.GetKey("a") || Input.GetKey("left"))
+        else if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            rigidBody.velocity = new Vector2(-runSpeed, rigidBody.velocity.y);
-
-            if(isGrounded)
-                animator.Play("Player_run");
-
+            rb2d.velocity = new Vector2(-runSpeed, rb2d.velocity.y);
+            queuedAnim = "Player_run";
             spriteRenderer.flipX = true;
         }
         else
         {
-            if (isGrounded)
-                animator.Play("Player_idle");
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
 
-            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        if (!isGrounded)
+        {
+            queuedAnim = "Player_jump";
         }
 
         if (Input.GetKey("space") && isGrounded)
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
-            animator.Play("Player_jump");
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
         }
+
+        if (Input.GetButtonDown("Fire1") && isGrounded)
+        {
+            animator.Play("Player_shoot");
+            GameObject bullet = (GameObject)Instantiate(bulletRef);
+            bullet.transform.position = new Vector3(transform.position.x + 3, transform.position.y + 1.8f, -1);
+        }
+        animator.Play(queuedAnim);
     }
 }
