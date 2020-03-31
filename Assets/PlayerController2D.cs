2e2aafeda01f05cab5546ba9dfee9916d7dac042
Haris Salman <haris.solo68@gmail.com>
2020-03-31 14:19:41 +0700

    add movement

--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -7,6 +7,20 @@ public class PlayerController2D : MonoBehaviour
     Animator animator;
     Rigidbody2D rigidBody;
     SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private float runSpeed = 5f;
+    [SerializeField]
+    private float jumpSpeed = 10f;
+
+    bool isGrounded;
+
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    Transform groundCheckL;
+    [SerializeField]
+    Transform groundCheckR;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,27 +32,48 @@ public class PlayerController2D : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        if(Input.GetKey("d") || Input.GetKey("right"))
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"))||
+            Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground")) ||
+            Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")))
+
         {
-            rigidBody.velocity = new Vector2(2, rigidBody.velocity.y);
-            animator.Play("Player_run");
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+            animator.Play("Player_jump");
+        }
+
+            if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
+
+            if(isGrounded)
+                animator.Play("Player_run");
+
             spriteRenderer.flipX = false;
         }
         else if(Input.GetKey("a") || Input.GetKey("left"))
         {
-            rigidBody.velocity = new Vector2(-2, rigidBody.velocity.y);
-            animator.Play("Player_run");
+            rigidBody.velocity = new Vector2(-runSpeed, rigidBody.velocity.y);
+
+            if(isGrounded)
+                animator.Play("Player_run");
+
             spriteRenderer.flipX = true;
         }
         else
         {
-            animator.Play("Player_idle");
+            if (isGrounded)
+                animator.Play("Player_idle");
+
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
 
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && isGrounded)
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 3);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
             animator.Play("Player_jump");
         }
     }
